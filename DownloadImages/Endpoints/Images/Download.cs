cf9ce8325a255bd1841f476bbd56ca3f7fae93f1
ashiqul <ashiqul33@gmail.com>
2026-06-09 23:59:18 +0600

    clean

--- DownloadImages/Endpoints/Images/Download.cs
+++ DownloadImages/Endpoints/Images/Download.cs
@@ -3,6 +3,7 @@ namespace DownloadImages.Endpoints.Images;
 internal sealed class Download : IEndpoint
 {
     public record RequestDownload(IEnumerable<string> ImageUrls, int MaxDownloadAtOnce);
+    public record ResponseDownload(bool Success, string? Message, IDictionary<string, string> UrlAndNames);
 
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
